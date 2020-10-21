0aa430df21892a0bf4f9fdfec3e6c4831722def7
coderInPractice <coderInPractice@noreply.github.com>
2020-10-22 01:13:29 +0530

    Code modified as per MVVM

--- app/src/main/java/com/example/favouritecaller/MainActivity.java
+++ app/src/main/java/com/example/favouritecaller/MainActivity.java
@@ -3,108 +3,58 @@ package com.example.favouritecaller;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.favouritecaller.adapter.MainActivityAdapter;
-import com.example.favouritecaller.model.ContactDbHelper;
-import com.example.favouritecaller.model.ContactModel;
+import com.example.favouritecaller.Adapters.MainActivityAdapter;
+import com.example.favouritecaller.Database.ContactModel;
+import com.example.favouritecaller.ViewModel.ContactViewModel;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity  {
     public static final String TAG = MainActivity.class.getName();
 
-   // ArrayList<ContactModel> contactList;
-    RecyclerView mainRecyclerview;
-    MainActivityAdapter mainAdapter;
-    Intent intent;
-    ContactDbHelper contactDbHelper = new ContactDbHelper(this);
+    RecyclerView savedRv;
+    MainActivityAdapter savedAdapter;
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
+    ContactViewModel mContactViewModel;
+
+
+    //@RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mainRecyclerview = findViewById(R.id.main_recycler_view);
+        getSupportActionBar().setElevation(8.0f);
 
-        intent = getIntent();
-        dataFromContactList(intent);
+        savedRv = findViewById(R.id.main_recycler_view);
+        mContactViewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()).create(ContactViewModel.class);
 
-        Cursor mCursor = contactDbHelper.getAllContacts();
-        mainAdapter = new MainActivityAdapter(this,mCursor);
+        savedRv.setLayoutManager(new GridLayoutManager(this,3));
+        savedAdapter = new MainActivityAdapter(this);
 
-        mainRecyclerview.setLayoutManager(new GridLayoutManager(this,3));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, 1);
+        } else {
+            getDataFromDb();
+        }
 
-        mainRecyclerview.setAdapter(mainAdapter);
-
-       if(checkSelfPermission(Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
-           requestPermissions(new String[] {Manifest.permission.CALL_PHONE},1);
-       }
-       else{
-           dataFromContactList(intent);
-       }
 
     }
 
-    private void dataFromContactList(Intent intent) {
-       // contactList = new ArrayList<>();
-        ContactModel contactModel;
-
-        Uri photoUri;
-
-        String name = intent.getStringExtra("contact_name");
-        Log.d(TAG, "dataFromContactList: name " + name);
-
-        String number = intent.getStringExtra("contact_number");
-        Log.d(TAG, "dataFromContactList: number " + number);
-
-        String photoString = intent.getStringExtra("photo_string");
-        Log.d(TAG, "dataFromContactList: photoString " + photoString);
-
-        if(photoString == null){
-            return;
-        }
-        else{
-            photoUri = Uri.parse(photoString);
-        }
-
-        int photoId = intent.getIntExtra("photo_string",R.drawable.person);
-        Log.d(TAG, "dataFromContactList: photoId " + photoId);
-
-        if(photoUri == null){
-            contactModel = new ContactModel(name,number,photoId);
-        }
-        else{
-            contactModel = new ContactModel(name,number,photoUri);
-        }
-
-        Log.d("contactModel: ", "dataFromContactList: " + contactModel.getPhotoId() + " " + contactModel.getName()
-                + " " + contactModel.getPhone_number() + " " + contactModel.getPhoto_uri());
-
-        contactDbHelper.addContacts(contactModel);
-
-
-        //contactList.add(contactModel);
-
-//        for(ContactModel  e : contactList){
-//            Log.d("Contact List: ", "dataFromContactList:  contactList Items: " + e.getName() + " " + e.getPhone_number() + " "  + e.getPhoto_uri() + " " + e.getPhotoId());
-//        }
-//
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -116,8 +66,8 @@ public class MainActivity extends AppCompatActivity  {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
-        Intent intent = new Intent(getApplicationContext(),ContactListActivity.class);
-        startActivity(intent);
+        Intent contact_list_intent = new Intent(this,ContactListActivity.class);
+        startActivity(contact_list_intent);
 
         return true;
     }
@@ -126,14 +76,17 @@ public class MainActivity extends AppCompatActivity  {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if(requestCode == 1){
             if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                dataFromContactList(intent);
+                getDataFromDb();
             }
         }
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mainAdapter.swapCursor(contactDbHelper.getAllContacts());
+    private void getDataFromDb() {
+
+        mContactViewModel.getAllContact().observe(this, contactModels -> {
+            savedAdapter.getList(contactModels);
+            savedRv.setAdapter(savedAdapter);
+        });
     }
+
 }
